94d27530508eec5a7f53c3f7b46cafd21bc275fd
xdshen <xdshent@gmail.com>
2019-03-06 21:35:58 +0800

    test: add TwoSumSolution test case
    1. TwoSum's TestCase

---
+++ src/test/java/com/github/xdshent/leetcode/array/TwoSumSolutionTest.java
@@ -0,0 +1,31 @@
+package com.github.xdshent.leetcode.array;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TwoSumSolutionTest {
+
+    private int[] array;
+
+    @Before
+    public void setUp() throws Exception {
+        array = new int[]{2, 7, 11, 15};
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        array = null;
+    }
+
+    @Test
+    public void twoSum() {
+        int target = 9;
+        int[] result = TwoSumSolution.twoSum(array, target);
+        Assert.assertTrue(result.length > 0);
+
+        int[] expected = {0, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+}
