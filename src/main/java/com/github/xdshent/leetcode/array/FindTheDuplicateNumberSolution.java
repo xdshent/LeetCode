8199268c8184968e7b37662a79ffd8bb3ce0229a
xdshen <xdshent@gmail.com>
2019-03-25 11:35:10 +0800

    feat: update 287. Find the Duplicate Number
    update 287. Find the Duplicate Number and Test Case

--- src/main/java/com/github/xdshent/leetcode/array/FindTheDuplicateNumberSolution.java
+++ src/main/java/com/github/xdshent/leetcode/array/FindTheDuplicateNumberSolution.java
@@ -1,13 +1,14 @@
 package com.github.xdshent.leetcode.array;
 
 /**
+ * 287. Find the Duplicate Number
  * Given an array nums containing n + 1 integers where each integer is between 1 and n(inclusive),
  * prove that at least one duplicate number must exist. Assume that there is only one duplicate.
- *
+ * <p>
  * Example 1:
  * Input: [1,3,4,2,2]
  * Output: 2
- *
+ * <p>
  * Example 2:
  * Input: [3,1,3,4,2]
  * Output: 3
@@ -22,7 +23,7 @@ public class FindTheDuplicateNumberSolution {
      * @param nums
      * @return
      */
-    public int findDuplicate(int[] nums) {
+    public int findDuplicateFirst(int[] nums) {
         if (nums == null || nums.length == 0 || nums.length == 1) {
             return -1;
         }
@@ -36,6 +37,54 @@ public class FindTheDuplicateNumberSolution {
             }
         }
         return -1;
+    }
+
+    /**
+     * @param nums
+     * @return
+     */
+    public int findDuplicateSecond(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1) {
+            return -1;
+        }
+
+        int left = 1;
+        int right = nums.length - 1;
+        int count = 0;
+
+        while (left <= right) {
+            int middle = left + ((right - left) >> 1);
+            count = countInRange(nums, left, middle);
+
+            if (left == right) {
+                if (count > 1) {
+                    return left;
+                }
+            }
+
+            if (count > middle - left + 1) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    private int countInRange(int[] nums, int left, int right) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= left && nums[i] <= right) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
--- src/test/java/com/github/xdshent/leetcode/array/FindTheDuplicateNumberSolutionTest.java
+++ src/test/java/com/github/xdshent/leetcode/array/FindTheDuplicateNumberSolutionTest.java
@@ -5,8 +5,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class FindTheDuplicateNumberSolutionTest {
 
     private int[] array;
@@ -24,29 +22,62 @@ public class FindTheDuplicateNumberSolutionTest {
     }
 
     @Test
-    public void findDuplicate() {
-
-        int result = findTheDuplicateNumberSolution.findDuplicate(array);
+    public void findDuplicateFirst() {
+        int result = findTheDuplicateNumberSolution.findDuplicateFirst(array);
         Assert.assertEquals(2, result);
 
         array = new int[]{3, 1, 3, 4, 2};
-        result = findTheDuplicateNumberSolution.findDuplicate(array);
+        result = findTheDuplicateNumberSolution.findDuplicateFirst(array);
         Assert.assertEquals(3, result);
 
         array = new int[]{5, 1, 3, 4, 2};
-        result = findTheDuplicateNumberSolution.findDuplicate(array);
+        result = findTheDuplicateNumberSolution.findDuplicateFirst(array);
         Assert.assertEquals(-1, result);
 
         array = null;
-        result = findTheDuplicateNumberSolution.findDuplicate(array);
+        result = findTheDuplicateNumberSolution.findDuplicateFirst(array);
         Assert.assertEquals(-1, result);
 
         array = new int[]{};
-        result = findTheDuplicateNumberSolution.findDuplicate(array);
+        result = findTheDuplicateNumberSolution.findDuplicateFirst(array);
         Assert.assertEquals(-1, result);
 
         array = new int[]{1};
-        result = findTheDuplicateNumberSolution.findDuplicate(array);
+        result = findTheDuplicateNumberSolution.findDuplicateFirst(array);
+        Assert.assertEquals(-1, result);
+    }
+
+    @Test
+    public void findDuplicateSecond() {
+        int result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
+        Assert.assertEquals(2, result);
+
+        array = new int[]{3, 1, 3, 4, 2};
+        result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
+        Assert.assertEquals(3, result);
+
+        array = new int[]{5, 1, 3, 4, 2};
+        result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
+        Assert.assertEquals(-1, result);
+
+        array = new int[]{3, 1, 1, 2};
+        result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
+        Assert.assertEquals(1, result);
+
+        array = new int[]{4, 1, 3, 2};
+        result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
+        Assert.assertEquals(-1, result);
+
+        array = null;
+        result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
+        Assert.assertEquals(-1, result);
+
+        array = new int[]{};
+        result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
+        Assert.assertEquals(-1, result);
+
+        array = new int[]{1};
+        result = findTheDuplicateNumberSolution.findDuplicateSecond(array);
         Assert.assertEquals(-1, result);
     }
 }
