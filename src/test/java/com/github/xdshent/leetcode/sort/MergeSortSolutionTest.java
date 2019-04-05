8892ea45a241bde15c56892ea784be739a8921b9
xdshen <xdshent@gmail.com>
2019-04-05 18:45:03 +0800

    feat: merge sort
    merge sort and test case

---
+++ src/test/java/com/github/xdshent/leetcode/sort/MergeSortSolutionTest.java
@@ -0,0 +1,48 @@
+package com.github.xdshent.leetcode.sort;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class MergeSortSolutionTest {
+
+    private MergeSortSolution mergeSortSolution;
+    private int[] array;
+
+    @Before
+    public void setUp() throws Exception {
+        mergeSortSolution = new MergeSortSolution();
+
+        array = new int[10000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100000);
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void mergeSort() {
+        int[] temp = Arrays.copyOf(array, array.length);
+        Arrays.sort(temp);
+        mergeSortSolution.mergeSort(array);
+        Assert.assertArrayEquals(temp, array);
+
+        array = new int[]{};
+        mergeSortSolution.mergeSort(array);
+        Assert.assertArrayEquals(array, array);
+
+        array = new int[]{1};
+        mergeSortSolution.mergeSort(array);
+        Assert.assertArrayEquals(array, array);
+
+        array = null;
+        mergeSortSolution.mergeSort(array);
+        Assert.assertArrayEquals(array, array);
+    }
+}
